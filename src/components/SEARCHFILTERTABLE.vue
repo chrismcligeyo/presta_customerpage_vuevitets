77be8134931699012bd670a505ad33304f77d43e
= <mcligeyochristopher@gmail.com>
2023-06-06 14:51:22 +0300

    tw-elements button working after installing w-elements package

--- src/components/SEARCHFILTERTABLE.vue
+++ src/components/SEARCHFILTERTABLE.vue
@@ -1,10 +1,79 @@
 <template>
-    <section class="">
- </section>
-  </template>
+  <section class="searchfiltertable">
+    <div class="flex flex-col">
+      <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+          <div class="overflow-hidden">
+            <table class="min-w-full text-left text-sm font-light">
+              <thead class="border-b font-medium dark:border-neutral-500">
+                <tr>
+                  <th scope="col" class="px-6 py-4">Name</th>
+                  <th scope="col" class="px-6 py-4">Phone Number</th>
+                  <th scope="col" class="px-6 py-4">Branch</th>
+                  <th scope="col" class="px-6 py-4">Sales Rep</th>
+                  <th scope="col" class="px-6 py-4">Approved</th>
+                  <th scope="col" class="px-6 py-4">Loan</th>
+                  <th scope="col" class="px-6 py-4">Ussd</th>
+                  <th scope="col" class="px-6 py-4">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                  <td class="whitespace-nowrap px-6 py-4 font-medium">John Kariuki</td>
+                  <td class="whitespace-nowrap px-6 py-4">0724534789</td>
+                  <td class="whitespace-nowrap px-6 py-4">HQ</td>
+                  <td class="whitespace-nowrap px-6 py-4">Marcus Oriela</td>
+                  <td class="whitespace-nowrap px-6 py-4 font-medium">500</td>
+                  <td class="whitespace-nowrap px-6 py-4">140,000</td>
+                  <td class="whitespace-nowrap px-6 py-4"><button
+                    type="button"
+                    class="inline-block rounded bg-danger  px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+                    Button
+                  </button></td>
+                  <td class="whitespace-nowrap px-6 py-4"><!-- Required font awesome -->
+                    <button
+                      type="button"
+                      data-te-ripple-init
+                      data-te-ripple-color="light"
+                      class="flex items-center rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        class="mr-1 h-4 w-4">
+                        <path
+                          fill-rule="evenodd"
+                          d="M19.5 21a3 3 0 003-3V9a3 3 0 00-3-3h-5.379a.75.75 0 01-.53-.22L11.47 3.66A2.25 2.25 0 009.879 3H4.5a3 3 0 00-3 3v12a3 3 0 003 3h15zm-6.75-10.5a.75.75 0 00-1.5 0v4.19l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V10.5z"
+                          clip-rule="evenodd" />
+                      </svg>
+                      Button
+                    </button></td>
+                </tr>
+                <tr
+                  class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                  <td class="whitespace-nowrap px-6 py-4 font-medium">2</td>
+                  <td class="whitespace-nowrap px-6 py-4">Jacob</td>
+                  <td class="whitespace-nowrap px-6 py-4">Thornton</td>
+                  <td class="whitespace-nowrap px-6 py-4">@fat</td>
+                </tr>
+                <tr
+                  class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                  <td class="whitespace-nowrap px-6 py-4 font-medium">3</td>
+                  <td class="whitespace-nowrap px-6 py-4">Larry</td>
+                  <td class="whitespace-nowrap px-6 py-4">Wild</td>
+                  <td class="whitespace-nowrap px-6 py-4">@twitter</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
   <script setup lang="ts">
-
-  </script>
+</script>
   
   <style scoped></style>
   
