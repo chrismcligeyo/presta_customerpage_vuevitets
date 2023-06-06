189c62f3bd507f1621785b885958b6121b54b2e8
= <mcligeyochristopher@gmail.com>
2023-06-06 13:27:00 +0300

    added tw-elements plugin plus conteny and darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,11 @@
 module.exports = {
   //content: ["./src/**/*.{html,js}"], this does not pick taiwind css,use below
   content: [ "./index.html",
-  "./src/**/*.{js,jsx,ts,tsx,vue}",],
+  "./src/**/*.{js,jsx,ts,tsx,vue}",
+  "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
 }
