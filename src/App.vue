8ccfe4b44858217a7c3d8fd230aca8fd96c79666
= <mcligeyochristopher@gmail.com>
2023-06-10 15:41:55 +0300

    changed searchfiltertable component to customerlist, working

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,36 @@
     <main class="bg-[#FFFFFF]">
       <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
-        <SEARCHFILTERTABLE/>
+        <CUSTOMERLISTS/>
       </div>
     </main>
   </div>
 </template>
 <script setup lang="ts">
 //import { RouterLink, RouterView } from 'vue-router'
+import { ref,  onMounted } from 'vue';
+import axios from "axios";
+
 
 import NAVBAR from "./components/NAVBAR.vue";
-import SEARCHFILTERTABLE from "./components/SEARCHFILTERTABLE.vue";
+import CUSTOMERLISTS from "./components/CUSTOMERLISTS.vue";
 import type {CustomerList} from "./types/CustomerList";
 
 
+const customerlists = ref<CustomerList[]>([]);  //customerlists is an empty array of customerlist objects
+
+async function loadcuctomerlists(){
+ const res = await axios.get("api/customerlists");
+ console.log(res);
+
+}
 
 
-
-
+onMounted(() => {
+  console.log(`the component is now mounted.`)
+  // customerlists.value = 
+  loadcuctomerlists();
+});
 </script>
 
 <style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-//import HomeView from '../views/HomeView.vue'
+import  CUSTOMERLISTS from '../components/CUSTOMERLISTS.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +17,12 @@ const router = createRouter({
     //   // which is lazy-loaded when the route is visited.
     //   component: () => import('../views/AboutView.vue')
     // }
+
+    //   {
+    //   path: '/customerlists',
+    //   name: 'customerlists',
+    //   component: CUSTOMERLISTS
+    // },
   ]
 })
 
